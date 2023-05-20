bb4f4dec21cad4fce80484d61f2d2c6a43e78527
lexkhortic <ringostarr1997083@gmail.com>
2023-05-20 20:07:02 +0300

    Updated search func

--- src/main/java/by/lexkhortic/recipeInPocket/controllers/IndexController.java
+++ src/main/java/by/lexkhortic/recipeInPocket/controllers/IndexController.java
@@ -1,6 +1,7 @@
 package by.lexkhortic.recipeInPocket.controllers;
 
 import by.lexkhortic.recipeInPocket.models.Medicine;
+
 import by.lexkhortic.recipeInPocket.models.Pharmacy;
 import by.lexkhortic.recipeInPocket.repositories.PharmacyRepository;
 import by.lexkhortic.recipeInPocket.services.IndexServices;
@@ -12,6 +13,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.*;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 @Controller
 public class IndexController {
@@ -27,27 +31,64 @@ public class IndexController {
             @RequestParam(name = "medicine_name", required = false) Set<String> medicinesList)
     {
         medicinesList.forEach(System.out::println);
-        Map<String, Set<Pharmacy>> medicinePharmaciesMap = new HashMap<>();
+
+        Map<String, Set<Long>> medicinePharmaciesMap = new HashMap<>();
 
         List<Medicine> medicinesID = indexServices.findAllIDMedicines(medicinesList);
 
         for (Medicine medicine : medicinesID) {
-            medicinePharmaciesMap.put(medicine.getName(), new HashSet<>());
+            medicinePharmaciesMap
+                    .computeIfAbsent(medicine.getName(), v -> new HashSet<>())
+                    .add(medicine.getPharmacyID());
         }
 
-        for (Medicine medicine : medicinesID) {
-            medicinePharmaciesMap.get(medicine.getName()).add(pharmacyRepository.getReferenceById(medicine.getPharmacyID()));
+        List<Long> pharmaciesID = new ArrayList<>();
+        for (Map.Entry<String, Set<Long>> entry : medicinePharmaciesMap.entrySet()) {
+            pharmaciesID.addAll(entry.getValue().stream().toList());
         }
 
-        System.out.println("Кол-во лекарств в Map - " + medicinePharmaciesMap.size());
-        for (Map.Entry<String, Set<Pharmacy>> entry : medicinePharmaciesMap.entrySet()) {
-            System.out.print(entry.getKey() + ": ");
-            entry.getValue().forEach(pharmacy -> {
-                System.out.print(pharmacy.getId() + " ");
+        pharmaciesID = pharmaciesID.stream()
+                .collect(Collectors.groupingBy(Function.identity()))
+                .entrySet()
+                .stream()
+                .filter(el -> el.getValue().size() == medicinesList.size())
+                .map(Map.Entry::getKey)
+                .collect(Collectors.toList());
+
+        pharmaciesID.forEach(System.out::println);
+
+        Map<Pharmacy, List<Medicine>> resultQuery = new HashMap<>();
+        for (Long id : pharmaciesID) {
+            Pharmacy pharmacy = pharmacyRepository.getReferenceById(id);
+            resultQuery
+                    .computeIfAbsent(pharmacy, v -> new ArrayList<>())
+                    .addAll(findAllMedicinesFromPharmacy(pharmacy, medicinesList));
+        }
+
+        for (Map.Entry<Pharmacy, List<Medicine>> entry : resultQuery.entrySet()) {
+            System.out.println(entry.getKey().getTitle() + ":");
+            entry.getValue().forEach(el -> {
+                System.out.println(el.getName() + ", " + el.getPrice() + " руб.");
             });
-            System.out.println();
         }
+
         return "find-medicines";
+    }
+
+    private List<Medicine> findAllMedicinesFromPharmacy(Pharmacy pharmacy, Set<String> medicineList) {
+        return pharmacy.getMedicinesList().stream()
+                .filter(el -> checkMedicineInList(el.getName(), medicineList))
+                .collect(Collectors.toList());
+    }
+
+    private boolean checkMedicineInList(String medicine, Set<String> medicineList) {
+        AtomicBoolean isHas = new AtomicBoolean(false);
+        medicineList.forEach(el -> {
+            if (el.equals(medicine)) {
+                 isHas.set(true);
+            }
+        });
+        return isHas.get();
     }
 
     @GetMapping("/")
